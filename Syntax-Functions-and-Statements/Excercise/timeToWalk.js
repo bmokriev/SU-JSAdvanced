15db8ffd3531798c8a7b1eb1a8cac2002f2f931d
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-01-28 19:17:47 +0200

    ex

--- Syntax-Functions-and-Statements/Excercise/timeToWalk.js
+++ Syntax-Functions-and-Statements/Excercise/timeToWalk.js
@@ -1,9 +1,33 @@
 function timeToWalk(steps, footprint, speed) {
-    let length = steps * footprint;
-    console.log(length);
+    let lengthM = steps * footprint;
+    let speedM = speed * 1000
+    let addedMins = Math.floor(lengthM / 500)
+    let timeMins = (lengthM * 60 / speedM + addedMins) * 10
+
+    let hours = Math.floor(timeMins / 600)
+    timeMins -= hours * 600
+    let minutes = Math.floor(timeMins / 10)
+    timeMins -= minutes * 10;
+    let seconds = timeMins * 6
+
+    let result = '';
+    let data = [hours, minutes, seconds]
+
+    for (let i = 0; i < 3; i++) {
+        if (data[i] < 10) {
+            result += '0'
+            result += data[i].toFixed()
+        } else {
+            result += data[i].toFixed()
+        }
+
+        if (i < 2) {
+            result += ':'
+        }
+    }
 
 
-
+    console.log(result)
 }
-timeToWalk(4000, 0.60, 5)
+// timeToWalk(4000, 0.60, 5)
 timeToWalk(2564, 0.70, 5.5)
