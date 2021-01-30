3f841fdad9a9e85e21d641e604c8c6d343f07db9
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-01-30 21:00:36 +0200

    ex

---
+++ Syntax-Functions-and-Statements/Excercise/validityChecker.js
@@ -0,0 +1,28 @@
+function validityCheck(x1, x2, y1, y2) {
+
+    function distance(xx1, yy1, xx2, yy2) {
+        let distH = xx1 - xx2;
+        let distV = yy1 - yy2;
+        return Math.sqrt(distH ** 2 + distV ** 2);
+    }
+
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+validityCheck(3, 0, 0, 4)
+validityCheck(10, 5, 2, 2)
