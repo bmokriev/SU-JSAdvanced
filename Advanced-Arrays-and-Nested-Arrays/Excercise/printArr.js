24aa4171e5d37e16fad51452b4071beb25883e09
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-06 11:10:53 +0200

    ex

---
+++ Advanced-Arrays-and-Nested-Arrays/Excercise/printArr.js
@@ -0,0 +1,19 @@
+function printArr(arr, delimiter) {
+    let newArr = arr.join(delimiter)
+    console.log(newArr);
+}
+printArr(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+)
+printArr(['How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'
+)
