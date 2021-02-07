2379f0c42e917bd8842b2ac0198762ec000a9399
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-07 12:24:47 +0200

    ex

---
+++ Objects-and-Composition/Lab/1-cityRecord.js
@@ -0,0 +1,13 @@
+function makeCity(name, pop, treasury) {
+    let obj = {
+        name: name,
+        population: pop,
+        treasury: treasury
+    };
+    return obj
+
+}
+console.log(makeCity('Tortuga',
+    7000,
+    15000
+));
