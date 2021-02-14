901564534d6f901b79766fb25728d88605eeec6d
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-14 18:47:35 +0200

    ex

--- DOM-Introduction/Excercises/02. Pascal or Camel Case/solution.js
+++ DOM-Introduction/Excercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value.toLowerCase();
+  // let text = "this is an example".toLowerCase();
+  let type = document.getElementById('naming-convention').value;
+  // let type = "Pascal Case"
+  let resultEl = document.getElementById('result')
+
+  if (type !== "Camel Case" && type !== "Pascal Case") {
+    resultEl.textContent = 'Error!'
+    return
+  }
+  let textArr = text.split(' ');
+
+  for (let i = 0; i < textArr.length; i++) {
+    if (i == 0 && type === "Camel Case") {
+      continue;
+    }
+    textArr[i] = textArr[i][0].toUpperCase() + textArr[i].slice(1)
+  }
+
+  let result = textArr.join('')
+
+  resultEl.textContent = result;
 }
