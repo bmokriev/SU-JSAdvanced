438914b8e3421e4a13aed514d4e3587fd3696a59
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-14 18:07:43 +0200

    ex

--- DOM-Introduction/Excercises/01. Subtraction/subtract.js
+++ DOM-Introduction/Excercises/01. Subtraction/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-    console.log('TODO:...');
+    let first = document.getElementById('firstNumber').value;
+    let second = document.getElementById('secondNumber').value;
+    let result = document.getElementById('result');
+    result.textContent = +first - +second;
+
+    console.log(result);
+
 }
