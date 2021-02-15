07978c5449fb7c08c3f2fbfa5465630027982370
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-15 19:18:26 +0200

    ex

--- DOM-Introduction/Excercises/05. Table - Serach Engine/solution.js
+++ DOM-Introduction/Excercises/05. Table - Serach Engine/solution.js
@@ -1,8 +1,16 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   const rows = document.querySelectorAll('tbody tr')
    function onClick() {
-      //   TODO:
+      let input = document.querySelector('#searchField').value;
+
+      for (const row of rows) {
+         if (row.textContent.includes(input)) {
+            row.classList.add('select');
+         } else {
+            row.classList.remove('select');
+         }
+      }
 
    }
 }
