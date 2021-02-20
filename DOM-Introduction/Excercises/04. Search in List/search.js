e3710daafdad6b30316fef0ba2dc9264baab64c5
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-20 12:41:09 +0200

    ex

--- DOM-Introduction/Excercises/04. Search in List/search.js
+++ DOM-Introduction/Excercises/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   // select els
+   let input = document.querySelector('#searchText').value;
+   let count = 0;
+   let resultText = document.querySelector('#result');
+
+   // search through them
+   // underline them
+   let towns = Array.from(document.querySelectorAll('li'))
+      .forEach(el => {
+         if (el.textContent.toLowerCase().includes(input.toLowerCase())) {
+            el.style.fontWeight = 'bold';
+            el.style.textDecoration = 'underline';
+            count++;
+         }
+      });
+
+   // print num of matches
+   resultText.textContent = `${count} matches found`
+
+
 }
