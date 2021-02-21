c68e773c288f3bf25d0e0825d252d40b8966abf8
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-21 21:56:41 +0200

    ex

--- DOM-Events/01. List-Of-Items/app.js
+++ DOM-Events/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let input = document.querySelector('#newItemText').value
+    let list = document.querySelector('#items')
+
+    let li = document.createElement('li');
+    li.textContent = input;
+    list.appendChild(li)
+
+
+
 }
