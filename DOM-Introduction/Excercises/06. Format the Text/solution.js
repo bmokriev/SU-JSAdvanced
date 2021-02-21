4f346df25b8aa62140ee6e311fba576aaee50d35
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-21 14:57:20 +0200

    ex

--- DOM-Introduction/Excercises/06. Format the Text/solution.js
+++ DOM-Introduction/Excercises/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  let input = document.querySelector('#input').value;
+  let output = document.querySelector('#output');
+  console.log(input);
+  input
+    .match(/(?:\s*)([^.!?]+[.!?]+)/g)
+    .reduce((acc, curr, i) => {
+      if (i % 3 == 0) {
+        acc.push([curr.trim()])
+      } else {
+        acc[acc.length - 1].push(curr)
+      }
+      return acc;
+    }, [])
+    .forEach(p => {
+      let par = document.createElement('p');
+      par.textContent = p;
+      output.appendChild(par)
+    });
+
 }
