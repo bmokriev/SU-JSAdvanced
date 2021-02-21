0643bdf1d116074167957f54c9dbdfeed96a6830
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-21 22:35:11 +0200

    ex

--- DOM-Events/03. Delete-From-Table/app.js
+++ DOM-Events/03. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let deleteVal = document.querySelector('input[name="email"]').value.trim();
+    const rows = Array.from(document.querySelectorAll('tbody tr'))
+
+    let msg = "Not found."
+    for (let row of rows) {
+        if (row.children[1].textContent === deleteVal) {
+            row.parentNode.removeChild(row)
+            msg = "Deleted"
+        }
+    }
+
+    document.querySelector('#result').textContent = msg;
 }
