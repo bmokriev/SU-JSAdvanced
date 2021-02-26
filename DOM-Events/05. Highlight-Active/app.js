96898e4f354a2260cd751716ab14b3247a8f0aaf
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-26 10:42:19 +0200

    ex

--- DOM-Events/05. Highlight-Active/app.js
+++ DOM-Events/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    // select all input fields
+    let divs = document.querySelectorAll('input[type=text]')
+        .forEach(x => {
+            x.addEventListener('focus', onFocus);
+            x.addEventListener('blur', onBlur);
+        })
+
+    // add event listener for focus
+    // apply focused on parent
+    function onFocus(ev) {
+        ev.target.parentNode.classList.add('focus')
+    }
+
+    // add event listener for blured
+    // remove focused on parent
+    function onBlur(ev) {
+        ev.target.parentNode.classList.remove('focus')
+    }
 }
