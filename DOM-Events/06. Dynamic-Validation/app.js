4b0bb382f992d4c76f49aed292005da304e2d14a
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-26 13:09:36 +0200

    ex

--- DOM-Events/06. Dynamic-Validation/app.js
+++ DOM-Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    console.log('TODO:...');
+    let email = document.getElementById('email')
+        .addEventListener('change', (ev) => {
+            let reg = /^[a-z]+@[a-z]+\.[a-z]+$/
+            if (reg.test(ev.target.value)) {
+                ev.target.classList.remove('error')
+            } else {
+                ev.target.classList.add('error')
+            }
+        })
 }
