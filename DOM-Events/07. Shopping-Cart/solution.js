20c60e8a994fba0c6e86e3bf53880da55724036b
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-27 21:00:15 +0200

    ex

--- DOM-Events/07. Shopping-Cart/solution.js
+++ DOM-Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-   //TODO...
+   let textarea = document.querySelector('textarea')
+
+   const cart = [];
+
+   document.querySelector('.shopping-cart')
+      .addEventListener('click', (ev) => {
+         if (ev.target.className === "add-product" && ev.target.tagName == "BUTTON") {
+            const product = ev.target.parentNode.parentNode;
+            const title = product.querySelector('.product-title').textContent;
+            const price = +product.querySelector('.product-line-price').textContent;
+            console.log(`Added ${title} for ${price.toFixed(2)} to the cart.\n`);
+            textarea.textContent += `Added ${title} for ${price.toFixed(2)} to the cart.\n`
+            cart.push({ title, price })
+         }
+      })
+
+   document.querySelector('.checkout').addEventListener('click', () => {
+      const result = cart.reduce((acc, c) => {
+         acc.items.push(c.title);
+         acc.total += c.price;
+         return acc;
+      }, { items: [], total: 0 });
+
+      textarea.value += `You bought ${result.items.join(', ')} for ${result.total.toFixed(2)}.`
+   })
+
 }
