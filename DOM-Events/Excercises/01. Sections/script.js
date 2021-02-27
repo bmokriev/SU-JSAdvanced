d83b110f2c315000ec8eaa0db52e0fce81959c17
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-27 21:30:04 +0200

    ex

--- DOM-Events/Excercises/01. Sections/script.js
+++ DOM-Events/Excercises/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.querySelector('#content')
+
+   for (const item of words) {
+      let divEl = document.createElement('div');
+      let pEl = document.createElement('p');
+      pEl.textContent = item;
+      pEl.style.display = 'none';
+      content.appendChild(divEl);
+      divEl.appendChild(pEl);
+   }
+
+   content.addEventListener('click', (ev) => {
+      if (ev.target.tagName === "DIV") {
+         console.log(ev.target);
+         ev.target.firstChild.style.display = 'block';
+      }
+   });
 }
