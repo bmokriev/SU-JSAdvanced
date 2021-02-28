264768b7aba6d1639b4e5fc33c6c06a6aaabf51c
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-02-28 12:40:47 +0200

    ex

--- DOM-Events/Excercises/03. Locked-Profile/app.js
+++ DOM-Events/Excercises/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const main = document.querySelector('#main')
+
+    main.addEventListener('click', (ev) => {
+        const parent = ev.target.parentNode;
+        const radioUnlocked = parent.querySelector('input[value="unlock"]')
+
+        if (radioUnlocked.checked == false) {
+            return
+        }
+
+        const divEl = parent.querySelector('div');
+        const isVisable = divEl.style.display === 'block';
+
+        if (ev.target.tagName === "BUTTON" && !isVisable) {
+            divEl.style.display = 'block';
+            ev.target.textContent = 'Hide it'
+        } else if (ev.target.tagName === "BUTTON") {
+            divEl.style.display = 'none';
+            ev.target.textContent = 'Show more'
+        }
+    });
 }
