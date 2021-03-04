c14c1b57bd2fdb1d54bcc48a205c6fe126678053
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-03-04 23:26:23 +0200

    ex

--- Unit-Testing-and-Error-Handling/Lab/4-sumOfNumbers/sumOfNumbers.test.js
+++ Unit-Testing-and-Error-Handling/Lab/4-sumOfNumbers/sumOfNumbers.test.js
@@ -5,6 +5,12 @@ const sum = require('./sumOfNumbers');
 
 describe('Sum numbers', () => {
     it('sums single number', () => {
-        expect([1]).to.equal(1);
+        expect(sum([1])).to.equal(1);
+    });
+    it('sums two numbers', () => {
+        expect(sum([1, 1])).to.equal(2);
+    });
+    it('sums different numbers', () => {
+        expect(sum([1, 2, 3])).to.equal(6);
     });
 });
